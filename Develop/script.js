18dcbf8408018a3c5de090444938f8cadfa7948c
mohamed-aden <aden6864@bears.unco.edu>
2022-10-31 09:54:36 -0600

    Completed the project

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
+var passwordText = document.querySelector('#password');
 
 const generatePassword = function () {
   const passwordLength = Number(
@@ -8,10 +9,8 @@ const generatePassword = function () {
 
   if (isNaN(passwordLength)) {
     alert(`Please enter a number`);
-    generatePassword();
   } else if (passwordLength < 8 || passwordLength > 128) {
     alert(`Password length should be between 8 - 128 characters`);
-    generatePassword();
   } else {
     const useLowerCase = confirm(`Do you want to include lowercase letters?`);
     const useUpperCase = confirm(`Do you want to include uppercase Letters?`);
@@ -19,45 +18,44 @@ const generatePassword = function () {
     const useSpecialCharacters = confirm(
       `Do you want to include special characters?`
     );
+
+    //Letters that will be used to change uppaerCase / LowerCase
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    const upperCaseLetters = [];
+    const lowerCaseLetters = [];
+    const numericDigits = [];
+    const specialCharacters = ['!', '@', '#', '%', '&', '*', '$'];
+
+    //Update the arrays
+    letters.forEach((letter) => {
+      upperCaseLetters.push(letter.toUpperCase());
+      lowerCaseLetters.push(letter.toLowerCase());
+    });
+
+    for (let i = 0; i < 10; i++) {
+      numericDigits.push(i);
+    }
+
+    //Options car for user requirements & update them with optional chaining
+    const optionsCart = [];
+
+    useLowerCase && optionsCart.push(lowerCaseLetters);
+    useUpperCase && optionsCart.push(upperCaseLetters);
+    useNumeric && optionsCart.push(numericDigits);
+    useSpecialCharacters && optionsCart.push(specialCharacters);
+
+    const newOptionsArr = optionsCart.flat();
+    console.log(newOptionsArr);
+
+    const passwordArr = [];
+
+    for (let i = 0; i < passwordLength; i++) {
+      const randomItem = Math.floor(Math.random() * newOptionsArr.length);
+      console.log(randomItem);
+      passwordArr.push(newOptionsArr[randomItem]);
+    }
   }
-
-  //Letters that will be used to change uppaerCase / LowerCase
-  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-  const upperCaseLetters = [];
-  const lowerCaseLetters = [];
-  const numericDigits = [];
-  const specialCharacters = ['!', '@', '#', '%', '&', '*', '$'];
-
-  //Update the arrays
-  letters.forEach((letter) => {
-    upperCaseLetters.push(letter.toUpperCase());
-    lowerCaseLetters.push(letter.toLowerCase());
-  });
-
-  for (let i = 0; i < 10; i++) {
-    numericDigits.push(i);
-  }
-
-  //Options car for user requirements & update them with optional chaining
-  const optionsCart = [];
-
-  useLowerCase && optionsCart.push(lowerCaseLetters);
-  useUpperCase && optionsCart.push(upperCaseLetters);
-  useNumeric && optionsCart.push(numericDigits);
-  useSpecialCharacters && optionsCart.push(specialCharacters);
-
-  const newOptionsArr = optionsCart.flat();
-  console.log(newOptionsArr);
-
-  const passwordArr = [];
-
-  for (let i = 0; i < passwordLength; i++) {
-    const randomItem = Math.floor(Math.random() * newOptionsArr.length);
-    console.log(randomItem);
-    passwordArr.push(newOptionsArr[randomItem]);
-  }
-
   return passwordArr.join('');
   // console.log(passwordArr);
 };
@@ -65,13 +63,11 @@ const generatePassword = function () {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   passwordText.value = password;
 
-  passwordText.value = '';
   console.log(`I am clicked`);
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-generatePassword('');
